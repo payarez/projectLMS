9d0253ef142015e30a7ea1ef736d03cd7834e4ad
payarez <esneidersalgado08@gmail.com>
2025-08-30 23:23:53 -0500

    primer commit (header y footer)

---
+++ src/app/components/layout/aside/aside.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside',
+  standalone: true,
+  imports: [],
+  templateUrl: './aside.html',
+  styleUrl: './aside.css'
+})
+export class Aside {
+
+}
